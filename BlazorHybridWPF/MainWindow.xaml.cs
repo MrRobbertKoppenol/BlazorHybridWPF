c616f2e9bba339db6c6852115d3a27841af15cc4
MrRobbertKoppenol <87547437+MrRobbertKoppenol@users.noreply.github.com>
2023-12-05 21:09:40 +0100

    Add project files.

---
+++ BlazorHybridWPF/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+
+namespace BlazorHybridWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            serviceCollection.AddMudServices();
+#if DEBUG
+            serviceCollection.AddBlazorWebViewDeveloperTools();
+#endif
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+        }
+    }
+}
